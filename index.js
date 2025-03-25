65983f1b5b01d833dadcd341cfa1f0b432c555ce
Berdia Bekauri <berdiabekauri@Berdias-MacBook-Air.local>
2025-03-25 21:24:03 +0400

    commit

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ async function fetchNews() {
 
 function displayNews(articles) {
     const container = document.getElementById("news-container");
+    container.innerHTML = ""; // Clear previous content
     articles.forEach(article => {
         const newsElement = document.createElement("div");
         newsElement.classList.add("news-item");
         newsElement.innerHTML = `
-            <img src="${article.urlToImage}" alt="News Image" />
+            <img src="${article.urlToImage || 'placeholder.jpg'}" alt="News Image" />
             <h3>${article.title}</h3>
             <p>${article.description || "No description available."}</p>
             <a href="${article.url}" target="_blank">Read more</a>
@@ -21,19 +22,11 @@ function displayNews(articles) {
         container.appendChild(newsElement);
     });
 }
-
-fetchNews()
+fetchNews();
 
 const checkbox = document.querySelector("input");
 const body = document.querySelector("body");
-const news = document.getElementsByClassName("news-item");
-const images = document.querySelector("img");
 
-const darkMode = () => {
-    body.style.background = "black";
-    body.style.color = "white";
-    news.style.borderColor = "white";
-    images.style.filter = "drop-shadow(0px 0px 20px white)";
-}
-
-checkbox.onclick = darkMode;
+checkbox.addEventListener("change", () => {
+    document.body.classList.toggle("dark-mode");
+});
